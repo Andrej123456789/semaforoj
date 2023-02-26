d78bd486e940f083ed6ef73756bb1b65ec8693dd
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-02-26 13:43:15 +0100

    Removed unused code

--- src/main.rs
+++ src/main.rs
@@ -5,44 +5,6 @@ use std::{thread, time::Duration};
 
 use crossterm::{style, QueueableCommand};
 
-fn draw_circle() {
-    let diameter = 7.0;
-    let radius = diameter / 2.0 - 0.5;
-
-    let mut r: f64 = radius + 0.25;
-    r = r.powf(2.0) + 1.0;
-
-    let mut r_min: f64 = radius - 1.0;
-    r_min = r_min.powf(2.0) + 1.0;
-
-    let mut result: String = String::new();
-
-    let mut i = 0.0;
-    while i < diameter {
-        let mut y: f64 = i - radius;
-        y = y.powf(2.0);
-
-        let mut j = 0.0;
-        while j < diameter {
-            let mut x: f64 = j - radius;
-            x = x.powf(2.0);
-
-            if r_min <= x && y <= r {
-                result += "* ";
-            } else {
-                result += "   ";
-            }
-
-            j += 1.0;
-        }
-
-        result += "\n";
-        i += 1.0;
-    }
-
-    println!("{}", result);
-}
-
 const SHAPE: &str = "
 *********************
 *                   *
